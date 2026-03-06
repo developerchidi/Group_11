1de53fd81b9675d6feee3094831ccc279a53cf68
Kim Nghi <Kimnghi0207@gmail.com>
2026-03-06 22:43:11 +0700

    components

---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
